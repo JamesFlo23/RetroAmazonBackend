56f27a62d1308ca1d78dca2d00b6ad89be370d24
James Florez <james_florez@insideranken.org>
2023-10-05 11:52:11 -0500

    push

--- routes/api/user.js
+++ routes/api/user.js
@@ -5,6 +5,20 @@ debugUser.color = '63';
 import {getUsers,addUser,loginUser} from '../../database.js';
 const router = express.Router();
 import bcrypt from 'bcrypt';
+import {validBody} from '../../middleware/validBody.js'
+import Joi from 'joi';
+
+//step 1: define new user schema
+const newUserSchema = Joi.object({
+  fullName:Joi.string().trim().min(1).max(50).required(),
+  password:Joi.string().trim().min(8).max(50).required(),
+  email:Joi.string().trim().email().required()
+});
+//step 2
+const loginUserSchema = Joi.object({
+  email:Joi.string().trim().email().required(),
+  password:Joi.string().trim().min(8).max(50).required()
+});
 
 router.get('/list', async (req,res) => {
   debugUser('Getting all users');
@@ -16,7 +30,7 @@ router.get('/list', async (req,res) => {
   }
 });
 
-router.post('/add',async (req,res) =>{
+router.post('/add',validBody(newUserSchema), async (req,res) =>{
   const newUser = req.body;
   newUser.password = await bcrypt.hash(newUser.password, 10);//this just hashes the new password
   try{
